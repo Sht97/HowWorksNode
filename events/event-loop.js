6e87c0cf74358a493c7e134151d6da33a74773e1
Daniel Rivera <Danielf.r97@gmail.com>
2020-01-13 17:47:54 -0500

    Refactor

--- events/event-loop.js
+++ events/event-loop.js
@@ -7,7 +7,7 @@ process.env.UV_THREADPOOL_SIZE = 4;
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
 
-fs.readFile("test-file.txt", () => {
+fs.readFile("../stream/test-file.txt", () => {
     console.log("I/O finished");
     console.log("----------------");
 
