46d6edd016a0dffd7897ca53c8849bb595e5e465
Daniel Rivera <Danielf.r97@gmail.com>
2020-01-29 08:26:01 -0500

    async

--- asynchronousJS/index.js
+++ asynchronousJS/index.js
@@ -1,14 +1,29 @@
 const fs=require('fs');
 const superagent=require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`,(err,data)=>{
-    console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err,res)=>{
-            console.log(res.body.message);
-            fs.writeFile('dog-img.txt',res.body.message,err=>{
-                console.log("Random image saved")
-            })
+const readFilePro=file=>{
+    return new Promise((resolve ,reject)=>{
+        fs.readFile(file,(err,data)=>{
+            if(err)reject('I could not find that file');
+            resolve(data);
+        })
+    })
+};
+
+const writeFilePro=(file,data)=>{
+    return new Promise(((resolve, reject) => {
+        fs.writeFile(file,data,err=>{
+            if(err) reject('We cant save the file');
+            resolve('Save successful')
+        })
+    }))
+}
+readFilePro(`${__dirname}/dog.txt`).then(result=>{
+
+    return superagent.get(`https://dog.ceo/api/breed/${result}/images/random`)}).then(res=>{
+        console.log(res.body.message);
+        return writeFilePro('dog-img.txt',res.body.message);
+    }).catch(err=>{
+        console.log(err.message);
     });
-});
